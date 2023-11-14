6d8386f4d18f069f44112310a660ffce1ae4b751
Antoine <hauret.antoine@gmail.com>
2023-11-14 22:42:25 +0100

    I added the Add and AddRange method on the Booking.cs

--- Domain/Entities/Booking.cs
+++ Domain/Entities/Booking.cs
@@ -13,4 +13,14 @@ public class Booking {
         //TODO : il faut la liaison avec trip pour pouvoir faire le calcul
         return 0.0;
     }
+    
+    private void Add(Passenger passenger) {
+        _passengers.Add(passenger);
+    }
+
+    private void AddRange(IEnumerable<Passenger> passengers) {
+        foreach (var p in passengers) {
+            Add(p);
+        }
+    }
 }
