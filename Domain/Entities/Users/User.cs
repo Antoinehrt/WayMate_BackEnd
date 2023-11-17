d00453c4fe1ced8eeca40d2f041c2d7acaf139de
NextAndCie <117162264+NextAndCie@users.noreply.github.com>
2023-11-17 17:34:08 +0100

    Modified birthdate - added regex verification

--- Domain/Entities/Users/User.cs
+++ Domain/Entities/Users/User.cs
@@ -1,12 +1,29 @@
-﻿namespace Domain.Entities.Users; 
+﻿using System.Text.RegularExpressions;
+
+namespace Domain.Entities.Users; 
 
 public class User {
-    private string _username { get; set; }
-    private string _password { get; set; }
-    private string _email { get; set; }
-    private string _birthdate { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    private string _birthdate;
+    public string Birthdate
+    {
+        get => _birthdate;
+        set
+        {
+            if(Regex.IsMatch(value,@"^((0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/(19[0-9][0-9]|20[0-9][0-9]))$")) 
+                _birthdate = value;
+            else 
+                throw new ArgumentException($"Date format not respected");
+        }
+    }
 
     public int CalculateAge(){
-        return DateTime.Today.Year - DateTime.Parse(_birthdate).Year;
+        //return DateTime.Today.Year - DateTime.Parse(Birthdate).Year;
+        double age = DateTime.Today.Subtract(
+            DateTime.Parse(Birthdate)
+        ).TotalDays / 365.2425;
+        return Convert.ToInt32(Math.Round(age));
     }
 }
