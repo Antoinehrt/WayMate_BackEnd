23a14bcf590d45f4a5c9655b2e0b56a384d8c1b3
SanoO0 <mathiasmanzano37@gmail.com>
2023-11-17 17:08:28 +0100

    Added Car in Infrastructure

---
+++ Infrastructure/Ef/Car/CarRepository.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Enums;
+using Infrastructure.Ef.DbEntities;
+
+namespace Infrastructure.Ef.Car;
+
+public class CarRepository : ICarRepository
+{
+    private readonly CarContext _context;
+
+    public CarRepository(CarContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<DbCar> FetchAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public DbCar FetchById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public DbCar Create(string numberPlate, string brand, string model, int nbSeats, FuelType fuelType, CarType carType)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Update(int id, string numberPlate, string brand, string model, int nbSeats, FuelType fuelType, CarType carType)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Infrastructure/Ef/DbEntities/DbCar.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Enums;
+
+namespace Infrastructure.Ef.DbEntities;
+
+public class DbCar
+{
+    public string NumberPlate { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int NbSeats { get; set; }
+    public FuelType FuelType { get; set; }
+    public CarType CarType { get; set; }
+}
