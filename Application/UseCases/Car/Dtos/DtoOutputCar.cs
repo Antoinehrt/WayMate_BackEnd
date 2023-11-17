99b7afddac1beb7937588f897c07e30e7f17aeba
SanoO0 <mathiasmanzano37@gmail.com>
2023-11-17 17:44:11 +0100

    Dto input/output/update for Car

---
+++ Application/UseCases/Car/Dtos/DtoOutputCar.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Enums;
+
+namespace Application.UseCases.Car.Dtos;
+
+public class DtoOutputCar
+{
+    public string NumberPlate { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int NbSeats { get; set; }
+    public FuelType FuelType { get; set; }
+    public CarType CarType { get; set; }
+}
