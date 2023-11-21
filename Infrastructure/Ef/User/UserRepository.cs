71966cd5c31f2d2514039201258e2c4b0a87afdc
SanoO0 <mathiasmanzano37@gmail.com>
2023-11-21 19:27:12 +0100

    Patch error in UserRepository
    User variable replace with Users

--- Infrastructure/Ef/User/UserRepository.cs
+++ Infrastructure/Ef/User/UserRepository.cs
@@ -13,20 +13,21 @@ public class UserRepository : IUserRepository
 
     public IEnumerable<DbUser> FetchAll()
     {
-        return _context.User.ToList();
+        return _context.Users.ToList();
     }
 
     public DbUser FetchById(int id)
     {
-        var user = _context.User.FirstOrDefault(u => u.Id == id);
+        var user = _context.Users.FirstOrDefault(u => u.Id == id);
 
         if (user == null) throw new KeyNotFoundException($"User with id{id} has not been found");
+        return user;
     }
 
     public DbUser Create(string username, string password, string email, string birthdate, bool isbanned)
     {
-        var user = new DbUser { Username = username, Password = password, Email = email, Birthdate = birthdate, IsBanned = isbanned };
-        _context.User.Add(user);
+        var user = new DbUser { Username = username, Password = password, Email = email, BirthDate = birthdate, IsBanned = isbanned };
+        _context.Users.Add(user);
         _context.SaveChanges();
         return user;
     }
