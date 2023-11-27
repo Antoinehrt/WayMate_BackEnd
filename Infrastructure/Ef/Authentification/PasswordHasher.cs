63b6c65fddddfd3537b59e265ee1004feeb12adc
Antoine <hauret.antoine@gmail.com>
2023-11-27 18:55:41 +0100

    Adding the method to hash the password and to verify the password

--- Infrastructure/Ef/Authentification/PasswordHasher.cs
+++ Infrastructure/Ef/Authentification/PasswordHasher.cs
@@ -1,14 +1,26 @@
-﻿namespace Infrastructure.Ef.Authentification; 
+﻿using BCrypt.Net;
+
+namespace Infrastructure.Ef.Authentification; 
 
 public class PasswordHasher : IPasswordHasher {
     
+    private int _COST = 12;
+    private HashType _hashType = HashType.SHA384; 
     
     
     public string HashPwd(string pwd) {
-        throw new NotImplementedException();
+        //D'après le READ.ME sur le github de la librairie, un salt est généré automatiquement en fonction du cost de donné en arg.
+        //Et comme dans toutes les librairies BCrypt, le salt est stocké avec le pwd. 
+        //Donc pas besoin de faire une nouvelle colonne, mais du coup je ne suis pas sur de correcpondre à ce que le prof à dit.
+        
+        
+        //return BCrypt.Net.BCrypt.EnhancedHashPassword(pwd, workFactor: _COST, _hashType);
+        return BCrypt.Net.BCrypt.HashPassword(pwd, workFactor: _COST);
+        
     }
 
-    public bool VerifyPwd(string pwdHash, string inputPwd) {
-        throw new NotImplementedException();
+    public bool VerifyPwd(string hashedPwd, string inputPwd) {
+        //return BCrypt.Net.BCrypt.EnhancedVerify(inputPwd, hashedPwd, _hashType);
+        return BCrypt.Net.BCrypt.Verify(inputPwd, hashedPwd);
     }
 }
