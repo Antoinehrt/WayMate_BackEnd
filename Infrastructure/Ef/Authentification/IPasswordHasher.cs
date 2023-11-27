c5ef5b46af2aeca3db615a558ddb9431d77ef013
Antoine <hauret.antoine@gmail.com>
2023-11-27 17:22:03 +0100

    Creation of files
    Creation of the files that will be used in the hashing and verifying of password

---
+++ Infrastructure/Ef/Authentification/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Ef.Authentification; 
+
+public interface IPasswordHasher {
+
+    string HashPwd(string pwd);
+    bool VerifyPwd(string pwdHash, string inputPwd);
+
+}
---
+++ Infrastructure/Ef/Authentification/PasswordHasher.cs
@@ -0,0 +1,14 @@
+﻿namespace Infrastructure.Ef.Authentification; 
+
+public class PasswordHasher : IPasswordHasher {
+    
+    
+    
+    public string HashPwd(string pwd) {
+        throw new NotImplementedException();
+    }
+
+    public bool VerifyPwd(string pwdHash, string inputPwd) {
+        throw new NotImplementedException();
+    }
+}
