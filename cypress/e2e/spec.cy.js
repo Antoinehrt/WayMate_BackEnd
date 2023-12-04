d33e1eb4a084c2c3c62d2246c25a44c081e34123
Antoine <hauret.antoine@gmail.com>
2023-12-05 00:21:39 +0100

    Suppression of the cypressFiles

--- cypress/e2e/spec.cy.js
+++
@@ -1,9 +0,0 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    cy.visit('https://localhost:7206/swagger/index.html')
-    cy.contains('GET').click()
-    cy.contains('Try it out').click()
-    cy.contains('Execute').click()
-    //expect(true).to.equal(true)
-  })
-})
