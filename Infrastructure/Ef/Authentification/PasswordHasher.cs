bfa5f7a1d73025497b173e1accfebe61dc56bea8
Antoine <hauret.antoine@gmail.com>
2023-12-04 16:34:51 +0100

    Suppression of comments

--- Infrastructure/Ef/Authentification/PasswordHasher.cs
+++ Infrastructure/Ef/Authentification/PasswordHasher.cs
@@ -9,18 +9,11 @@ public class PasswordHasher : IPasswordHasher {
     
     
     public string HashPwd(string pwd) {
-        //D'après le READ.ME sur le github de la librairie, un salt est généré automatiquement en fonction du cost de donné en arg.
-        //Et comme dans toutes les librairies BCrypt, le salt est stocké avec le pwd. 
-        //Donc pas besoin de faire une nouvelle colonne, mais du coup je ne suis pas sur de correcpondre à ce que le prof à dit.
-        
-        
-        //return BCrypt.Net.BCrypt.EnhancedHashPassword(pwd, workFactor: _COST, _hashType);
         return BCrypt.Net.BCrypt.HashPassword(pwd, workFactor: _COST);
         
     }
 
     public bool VerifyPwd(string hashedPwd, string inputPwd) {
-        //return BCrypt.Net.BCrypt.EnhancedVerify(inputPwd, hashedPwd, _hashType);
         return BCrypt.Net.BCrypt.Verify(inputPwd, hashedPwd);
     }
 }
