7d5b113b5de95e32e31980b062470f28f8311032
Antoine <hauret.antoine@gmail.com>
2023-12-07 16:44:55 +0100

    Suppress unused DTO

--- Application/UseCases/Authentication/Dtos/DtoInputLogin.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Application.UseCases.Authentication.Dtos;
-
-public class DtoInputLogin {
-    [Required] public string Email { get; set; }
-    [Required] public string Password { get; set; }
-}
