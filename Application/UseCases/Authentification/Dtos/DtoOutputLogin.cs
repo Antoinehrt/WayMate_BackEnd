ff221e18491d630fc3dd761b055328a82a4a07b2
Antoine <hauret.antoine@gmail.com>
2023-12-07 15:13:00 +0100

    Creation of the files used for login

---
+++ Application/UseCases/Authentification/Dtos/DtoOutputLogin.cs
@@ -0,0 +1,5 @@
+﻿namespace Application.UseCases.Authentification.Dtos; 
+
+public class DtoOutputLogin {
+    
+}
---
+++ Application/UseCases/Authentification/UseCaseLogin.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.UseCases.Authentification; 
+
+public class UseCaseLogin {
+    // Cette classe devrait renvoyer le TOKEN JWT pour spécifier que l'utilisateur est connecté.
+    
+}
