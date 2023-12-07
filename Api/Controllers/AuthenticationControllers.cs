8492378825f47667f5e712552827380980aff370
Antoine <hauret.antoine@gmail.com>
2023-12-07 16:57:15 +0100

    Modification of the API route

--- Api/Controllers/AuthenticationControllers.cs
+++ Api/Controllers/AuthenticationControllers.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Api.Controllers;
 
 [ApiController]
-[Route("api/v1/login")]
+[Route("api/v1/authentication")]
 public class AuthenticationControllers : ControllerBase {
     private readonly UseCaseLogin _useCaseLogin;
 
