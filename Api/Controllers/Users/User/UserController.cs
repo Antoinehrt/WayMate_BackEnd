3b4117fd6092f48e1e572708d2753abd1d8e8c8a
Antoine <hauret.antoine@gmail.com>
2023-12-07 15:38:22 +0100

    Reformat the code UserController.cs

--- Api/Controllers/Users/User/UserController.cs
+++ Api/Controllers/Users/User/UserController.cs
@@ -7,19 +7,20 @@ namespace Api.Controllers.Users.User;
 
 [ApiController]
 [Route("api/v1/user")]
-public class UserController : ControllerBase
-{
+public class UserController : ControllerBase {
     private readonly UseCaseCreateUser _useCaseCreateUser;
     private readonly UseCaseFetchAllUser _useCaseFetchAllUser;
     private readonly UseCaseFetchUserById _useCaseFetchUserById;
+    private readonly UserCaseFetchUserByEmail _userCaseFetchUserByEmail;
+    private readonly UseCaseDeleteUser _useCaseDeleteUser;
+    private readonly UseCaseUpdateUser _useCaseUpdateUser;
 
-    public UserController(UseCaseCreateUser useCaseCreateUser, 
-        UseCaseFetchAllUser useCaseFetchAllUser, 
+    public UserController(UseCaseCreateUser useCaseCreateUser,
+        UseCaseFetchAllUser useCaseFetchAllUser,
         UseCaseFetchUserById useCaseFetchUserById,
         UserCaseFetchUserByEmail userCaseFetchUserByEmail,
-        UseCaseDeleteUser useCaseDeleteUser, 
-        UseCaseUpdateUser useCaseUpdateUser)
-    {
+        UseCaseDeleteUser useCaseDeleteUser,
+        UseCaseUpdateUser useCaseUpdateUser) {
         _useCaseCreateUser = useCaseCreateUser;
         _useCaseFetchAllUser = useCaseFetchAllUser;
         _useCaseFetchUserById = useCaseFetchUserById;
@@ -28,82 +29,68 @@ public class UserController : ControllerBase
         _useCaseUpdateUser = useCaseUpdateUser;
     }
 
-    private readonly UserCaseFetchUserByEmail _userCaseFetchUserByEmail;
-
-    private readonly UseCaseDeleteUser _useCaseDeleteUser;
-
-    private readonly UseCaseUpdateUser _useCaseUpdateUser;
-
     [HttpGet]
-    public ActionResult<IEnumerable<DtoOutputUser>> FetchAll()
-    {
+    public ActionResult<IEnumerable<DtoOutputUser>> FetchAll() {
         return Ok(_useCaseFetchAllUser.Execute());
     }
-    
-   [HttpGet]
-   [Route("{id:int}")]
-   [ProducesResponseType(StatusCodes.Status200OK)]
-   [ProducesResponseType(StatusCodes.Status404NotFound)]
-   public ActionResult<DtoOutputUser> FetchById(int id)
-   {
-       try
-       {
-           return _useCaseFetchUserById.Execute(id);
-       }
-       catch (KeyNotFoundException e)
-       {
-           return NotFound(new
-           {
-               e.Message
-           });
-       }
-   }
 
-   [HttpGet]
-   [Route("{email}")]
-   [ProducesResponseType(StatusCodes.Status200OK)]
-   [ProducesResponseType(StatusCodes.Status404NotFound)]
-   public ActionResult<DtoOutputUser> FetchByEmail(string email) {
-       try {
-           var result =  _userCaseFetchUserByEmail.Execute(email);
-           if (result == null) return NotFound(new { Message = "User not found" });
-           return result;
-       }
-       catch (KeyNotFoundException e) {
-           return NotFound(new {
-               e.Message
-           });
-       }
-   }
-   
-   
-   [HttpPost]
-   [ProducesResponseType(StatusCodes.Status201Created)]
-   public ActionResult<DtoOutputUser> Create(DtoInputCreateUser dto)
-   {
-       var output = _useCaseCreateUser.Execute(dto);
-       return CreatedAtAction(
-           nameof(FetchById),
-           new { id = output.Id },
-           output
-       );
-   }
+    [HttpGet]
+    [Route("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<DtoOutputUser> FetchById(int id) {
+        try {
+            return _useCaseFetchUserById.Execute(id);
+        }
+        catch (KeyNotFoundException e) {
+            return NotFound(new {
+                e.Message
+            });
+        }
+    }
 
-   [HttpDelete("{id:int}")]
-   [ProducesResponseType(StatusCodes.Status204NoContent)]
-   [ProducesResponseType(StatusCodes.Status404NotFound)]
-   public ActionResult Delete(int id)
-   {
-       if(_useCaseDeleteUser.Execute(id)) return NoContent();
-       return NotFound();
-   }
+    [HttpGet]
+    [Route("{email}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<DtoOutputUser> FetchByEmail(string email) {
+        try {
+            var result = _userCaseFetchUserByEmail.Execute(email);
+            if (result == null) return NotFound(new { Message = "User not found" });
+            return result;
+        }
+        catch (KeyNotFoundException e) {
+            return NotFound(new {
+                e.Message
+            });
+        }
+    }
 
-   [HttpPut("{id:int}")]
-   [ProducesResponseType(StatusCodes.Status204NoContent)]
-   [ProducesResponseType(StatusCodes.Status404NotFound)]
-   public ActionResult Update(int id, [FromBody] DtoInputUpdateUser dto)
-   {
-       dto.Id = id;
-       return _useCaseUpdateUser.Execute(dto) ? NoContent() : NotFound();
-   }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public ActionResult<DtoOutputUser> Create(DtoInputCreateUser dto) {
+        var output = _useCaseCreateUser.Execute(dto);
+        return CreatedAtAction(
+            nameof(FetchById),
+            new { id = output.Id },
+            output
+        );
+    }
+
+    [HttpDelete("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult Delete(int id) {
+        if (_useCaseDeleteUser.Execute(id)) return NoContent();
+        return NotFound();
+    }
+
+    [HttpPut("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult Update(int id, [FromBody] DtoInputUpdateUser dto) {
+        dto.Id = id;
+        return _useCaseUpdateUser.Execute(dto) ? NoContent() : NotFound();
+    }
 }
