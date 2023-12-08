ba8301399de6012666a4c33d217fc943a6a6c9cc
NextAndCie <117162264+NextAndCie@users.noreply.github.com>
2023-12-08 15:17:49 +0100

    Modified sql files - Merged users types

--- WayMate.sql
+++ WayMate.sql
@@ -4,39 +4,29 @@ GO
 USE WayMate;
 GO
 
-drop table dbo.admin
+drop table dbo.admin;
 go
 
-drop table dbo.booking
+drop table dbo.booking;
 go
 
-drop table dbo.trip
+drop table dbo.trip;
 go
 
-drop table dbo.driver
+drop table dbo.driver;
 go
 
-drop table dbo.car
+drop table dbo.car;
 go
 
-drop table dbo.passenger
+drop table dbo.passenger;
 go
 
-drop table dbo.address
+drop table dbo.address;
 go
 
-drop table dbo.users
+drop table dbo.users;
 go
-
--- Creating the 'users' table
-CREATE TABLE users (
-    id INT IDENTITY PRIMARY KEY NOT NULL,
-    username VARCHAR(20) NOT NULL,
-    password VARCHAR(200) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    birthDate DATE NOT NULL,
-    isBanned BIT NOT NULL DEFAULT 0
-);
 
 -- Creating the 'address' table
 CREATE TABLE address (
@@ -45,16 +35,6 @@ CREATE TABLE address (
     postalCode VARCHAR(6) NOT NULL,
     city VARCHAR(50) NOT NULL,
     number VARCHAR(6) NOT NULL
-);
-
--- Creating the 'passenger' table
-CREATE TABLE passenger (
-    id INT IDENTITY PRIMARY KEY NOT NULL,
-    userId INT NOT NULL REFERENCES users(id),
-    lastName VARCHAR(50) NOT NULL,
-    firstName VARCHAR(50) NOT NULL,
-    gender VARCHAR(20) NOT NULL,
-    addressId INT NOT NULL REFERENCES address(id)
 );
 
 -- Creating the 'car' table
@@ -67,23 +47,26 @@ CREATE TABLE car (
     fuelType INT NOT NULL DEFAULT 0
 );
 
--- Creating the 'driver' table
-CREATE TABLE driver (
+-- Creating the 'users' table
+CREATE TABLE users (
     id INT IDENTITY PRIMARY KEY NOT NULL,
-    passengerId INT NOT NULL REFERENCES passenger(id),
-    carPlate VARCHAR(50) NOT NULL REFERENCES car(plateNumber)
-);
-
--- Creating the 'admin' table
-CREATE TABLE admin (
-    id INT IDENTITY PRIMARY KEY NOT NULL,
-    idUser INT NOT NULL REFERENCES users(id)
+    userType VARCHAR(10) NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    birthDate DATE NOT NULL,
+    isBanned BIT NOT NULL DEFAULT 0,
+    lastName VARCHAR(50),
+    firstName VARCHAR(50),
+    gender VARCHAR(20),
+    addressId INT REFERENCES address(id),
+    carPlate VARCHAR(50) REFERENCES car(plateNumber)
 );
 
 -- Creating the 'trip' table
 CREATE TABLE trip (
     id INT IDENTITY PRIMARY KEY NOT NULL,
-    idDriver INT NOT NULL REFERENCES driver(id),
+    idDriver INT NOT NULL REFERENCES users(id),
     smoke BIT NOT NULL,
     priceKm FLOAT NOT NULL,
     luggage BIT NOT NULL,
@@ -99,7 +82,7 @@ CREATE TABLE booking (
     id INT IDENTITY PRIMARY KEY NOT NULL,
     date DATETIME NOT NULL,
     reservedSeats INT NOT NULL,
-    idPassenger INT NOT NULL REFERENCES passenger(id),
+    idPassenger INT NOT NULL REFERENCES users(id),
     idEntryPoint INT NOT NULL REFERENCES address(id),
     idTrip INT NOT NULL REFERENCES trip(id)
 );
