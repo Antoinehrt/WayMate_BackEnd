c6215c82f0db507ca341904e640f8cad58a9ec5c
Antoine <59895814+Antoinehrt@users.noreply.github.com>
2023-12-11 19:40:35 +0100

    Creation of create method in AdminController.cs

--- Api/Controllers/Users/AdminController.cs
+++ Api/Controllers/Users/AdminController.cs
@@ -1,5 +1,28 @@
-﻿namespace Api.Controllers.Users; 
+﻿using Application.UseCases.Users.Admin;
+using Application.UseCases.Users.Admin.Dtos;
+using Application.UseCases.Users.User.Dtos;
+using Microsoft.AspNetCore.Mvc;
 
+namespace Api.Controllers.Users; 
+
+[ApiController]
+[Route("api/v1/admin")]
 public class AdminController {
+    private readonly UseCaseCreateAdmin _useCaseCreateAdmin;
+
+    public AdminController(UseCaseCreateAdmin useCaseCreateAdmin) {
+        _useCaseCreateAdmin = useCaseCreateAdmin;
+    }
+    
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public ActionResult<DtoOutputAdmin> Create(DtoInputCreateAdmin dto) {
+        var output = _useCaseCreateAdmin.Execute(dto);
+        return CreatedAtAction(
+            nameof(FetchById),
+            new { id = output.Id },
+            output
+        );
+    }
     
 }
