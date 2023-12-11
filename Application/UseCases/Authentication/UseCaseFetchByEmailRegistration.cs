a8061f08425d5623cfeac1854119aea9210eb4f3
SanoO0 <mathiasmanzano37@gmail.com>
2023-12-12 00:31:30 +0100

    Create UseCaseFetchByEmail and username Registration

---
+++ Application/UseCases/Authentication/UseCaseFetchByEmailRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Application.UseCases.Authentication.Dtos;
+using AutoMapper;
+using Infrastructure.Ef.Users.User;
+
+namespace Application.UseCases.Authentication;
+
+public class UseCaseFetchByEmailRegistration
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+
+    public UseCaseFetchByEmailRegistration(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public DtoOutputRegistration Execute(string username)
+    {
+        var dbUser = _userRepository.FetchByUsername(username);
+        return _mapper.Map<DtoOutputRegistration>(dbUser);
+    }
+}
---
+++ Application/UseCases/Authentication/UseCaseFetchByUsernameRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Application.UseCases.Authentication.Dtos;
+using AutoMapper;
+using Infrastructure.Ef.Users.User;
+
+namespace Application.UseCases.Authentication;
+
+public class UseCaseFetchByUsernameRegistration
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+
+    public UseCaseFetchByUsernameRegistration(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public DtoOutputRegistration Execute(string email)
+    {
+        var dbUser = _userRepository.FetchByEmail(email);
+        return _mapper.Map<DtoOutputRegistration>(dbUser);
+    }
+}
