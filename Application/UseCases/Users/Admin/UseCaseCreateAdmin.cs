69ddea8f49162c03ead656a574f8285c2d807d28
Antoine <hauret.antoine@gmail.com>
2023-12-11 17:59:10 +0100

    Creation of the use case to create an admin

---
+++ Application/UseCases/Users/Admin/UseCaseCreateAdmin.cs
@@ -0,0 +1,26 @@
+﻿using Application.UseCases.Users.Admin.Dtos;
+using Application.UseCases.Users.User.Dtos;
+using Application.UseCases.Utils;
+using AutoMapper;
+using Infrastructure.Ef.Users.Admin;
+using Infrastructure.Ef.Users.User;
+
+namespace Application.UseCases.Users.Admin;
+
+public class UseCaseCreateAdmin : IUseCaseWriter<DtoOutputUser, DtoInputCreateAdmin>
+{
+    private readonly IAdminRepository _adminRepository;
+    private readonly IMapper _mapper;
+
+    public UseCaseCreateAdmin(IMapper mapper, IAdminRepository adminRepository)
+    {
+        _mapper = mapper;
+        _adminRepository = adminRepository;
+    }
+
+    public DtoOutputUser Execute(DtoInputCreateAdmin input)
+    {
+        var dbUser = _adminRepository.CreateAdmin(input.Username, input.Password, input.Email, input.Birthdate, input.IsBanned, input.PhoneNumber);
+        return _mapper.Map<DtoOutputUser>(dbUser);
+    }
+}
