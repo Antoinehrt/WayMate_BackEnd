5af89a31bea364d61f45ad9da8a6b0069a2fa68b
Antoine <59895814+Antoinehrt@users.noreply.github.com>
2023-12-12 15:50:59 +0100

    Creation of IUserRepository.cs and UserRepository.cs

--- Infrastructure/Ef/Users/User/IUserRepository.cs
+++ Infrastructure/Ef/Users/User/IUserRepository.cs
@@ -7,9 +7,12 @@ public interface IUserRepository
     IEnumerable<DbUser> FetchAll();
     DbUser FetchById(int id);
     DbUser FetchByEmail(string email);
-    DbUser CreatePassenger(string username, string password, string email, DateTime birthdate, bool isbanned, string phoneNumber,
-        string lastName, string firstName, string gender, int addressId);
-    bool Delete(int id);
-    bool Update(int id, string username, string password, string email, DateTime birthdate, bool isbanned, string phoneNumber,
+
+    DbUser Create(string username, string password, string email, DateTime birthdate, bool isbanned, string phoneNumber,
         string lastName, string firstName, string gender, int addressId, string carPlate);
+
+    DbUser Update(int id, string username, string password, string email, DateTime birthdate, bool isbanned, string phoneNumber,
+        string lastName, string firstName, string gender, int addressId, string carPlate);
+
+    bool Delete(int id);
 }
--- Infrastructure/Ef/Users/User/UserRepository.cs
+++ Infrastructure/Ef/Users/User/UserRepository.cs
@@ -1,111 +1,65 @@
-﻿using Domain.Enums;
+﻿using System.Diagnostics.CodeAnalysis;
+using Domain.Enums;
+using Infrastructure.Ef.Address;
 using Infrastructure.Ef.Authentication;
 using Infrastructure.Ef.DbEntities;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Infrastructure.Ef.Users.User;
 
-public class UserRepository : IUserRepository
-{
+public class UserRepository : IUserRepository {
     private readonly WaymateContext _context;
     private readonly IPasswordHasher _passwordHasher;
+
     public UserRepository(WaymateContext context, IPasswordHasher passwordHasher) {
         _context = context;
         _passwordHasher = passwordHasher;
     }
 
-    public IEnumerable<DbUser> FetchAll()
-    {
+    public IEnumerable<DbUser> FetchAll() {
         return _context.Users.ToList();
     }
 
-    public DbUser FetchById(int id)
-    {
+    public DbUser FetchById(int id) {
         var user = _context.Users.FirstOrDefault(u => u.Id == id);
-
-        if (user == null) throw new KeyNotFoundException($"User with id{id} has not been found");
-        
+        if (user == null) throw new KeyNotFoundException($"User with id {id} has not been found");
         return user;
     }
 
     public DbUser FetchByEmail(string email) {
         var user = _context.Users.FirstOrDefault(u => u.Email == email);
-
         if (user == null) throw new KeyNotFoundException($"User with email {email} has not been found");
-
         return user;
     }
 
-    public DbUser CreateAdmin(string username, string password, string email, DateTime birthdate, bool isbanned, string phoneNumber) {
-        
+    public DbUser Create(string username, string password, string email, DateTime birthdate, bool isbanned,
+        string phoneNumber, string lastName, string firstName, string gender, int addressId, string carPlate) {
         var user = new DbUser {
-            Username = username, 
-            UserType = UserType.Admin.ToString(),
-            Password = _passwordHasher.HashPwd(password), 
-            Email = email, 
-            BirthDate = birthdate, 
-            IsBanned = isbanned,
-            PhoneNumber = phoneNumber
-        };
-        _context.Users.Add(user);
-        _context.SaveChanges();
-        return user;
-    }
-    
-    public DbUser CreatePassenger(string username, string password, string email, DateTime birthdate, bool isbanned, string phoneNumber,
-        string lastName, string firstName, string gender, int addressId) {
-        
-        var user = new DbUser {
-            Username = username, 
-            UserType = UserType.Passenger.ToString(),
-            Password = _passwordHasher.HashPwd(password), 
-            Email = email, 
-            BirthDate = birthdate, 
+            Username = username,
+            UserType = UserType.User.ToString(),
+            Password = _passwordHasher.HashPwd(password),
+            Email = email,
+            BirthDate = birthdate,
             IsBanned = isbanned,
             PhoneNumber = phoneNumber,
             LastName = lastName,
             FirstName = firstName,
             Gender = gender,
-            AddressId = addressId
+            AddressId = addressId,
+            CarPlate = carPlate
         };
         _context.Users.Add(user);
         _context.SaveChanges();
         return user;
     }
 
-    public bool Delete(int id)
-    {
-        var userToDelete = _context.Users.FirstOrDefault(u => u.Id == id);
-
-        if (userToDelete == null)
-        {
-            throw new KeyNotFoundException($"User with id {id} has not been found");
-        }
-
-        _context.Users.Remove(userToDelete);
-        _context.SaveChanges();
-
-        return true;
-    }
-
-    public bool Update(int id, string username, string password, string email, DateTime birthdate, bool isbanned, string phoneNumber,
-        string lastName, string firstName, string gender, int addressId, string? carPlate)
-    {
+    public DbUser Update(int id, string username, string password, string email, DateTime birthdate, bool isbanned,
+        string phoneNumber, string lastName, string firstName, string gender, int addressId, string carPlate) {
         var userToUpdate = _context.Users.FirstOrDefault(u => u.Id == id);
-
-        if (userToUpdate == null) {
-            throw new KeyNotFoundException($"User with id {id} has not been found");
-        }
-
-        if (userToUpdate.UserType != UserType.Admin.ToString())
-        {
-            if (carPlate != null)
-            {
-                userToUpdate.UserType = UserType.Driver.ToString();
-            }
-            else userToUpdate.UserType = UserType.Passenger.ToString();
-        }
-
+        if (userToUpdate == null) throw new KeyNotFoundException($"User with id {id} has not been found");
+        
         userToUpdate.Username = username;
+        userToUpdate.UserType = UserType.User.ToString();
         userToUpdate.Password = _passwordHasher.HashPwd(password);
         userToUpdate.Email = email;
         userToUpdate.BirthDate = birthdate;
@@ -117,6 +71,15 @@ public class UserRepository : IUserRepository
         userToUpdate.AddressId = addressId;
         userToUpdate.CarPlate = carPlate;
 
+        _context.SaveChanges();
+        return userToUpdate;
+    }
+
+    public bool Delete(int id) {
+        var userToDelete = _context.Users.FirstOrDefault(u => u.Id == id);
+        if (userToDelete == null) throw new KeyNotFoundException($"User with id {id} has not been found");
+
+        _context.Users.Remove(userToDelete);
         _context.SaveChanges();
         return true;
     }
