bfdf66b7a6f7cc4a5fd0e559aa946f0d3908c666
Antoine <hauret.antoine@gmail.com>
2023-12-12 12:10:58 +0100

    Creation of the Create and Update use case for passenger.

--- Application/UseCases/Users/Passenger/UseCaseCreatePassenger.cs
+++ Application/UseCases/Users/Passenger/UseCaseCreatePassenger.cs
@@ -1,25 +1,26 @@
-﻿using Application.UseCases.Users.User.Dtos;
+﻿using Application.UseCases.Users.Admin.Dtos;
+using Application.UseCases.Users.Passenger.Dtos;
+using Application.UseCases.Users.User.Dtos;
 using Application.UseCases.Utils;
 using AutoMapper;
+using Infrastructure.Ef.Authentication;
+using Infrastructure.Ef.Users.Passenger;
 using Infrastructure.Ef.Users.User;
 
-namespace Application.UseCases.Users.User;
+namespace Application.UseCases.Users.Passenger;
 
-public class UseCaseCreatePassenger : IUseCaseWriter<DtoOutputUser, DtoInputCreatePassenger>
-{
-    private readonly IUserRepository _userRepository;
+public class UseCaseCreatePassenger : IUseCaseWriter<DtoOutputPassenger, DtoInputCreatePassenger> {
+    private readonly IPassengerRepository _passengerRepository;
     private readonly IMapper _mapper;
 
-    public UseCaseCreatePassenger(IUserRepository userRepository, IMapper mapper)
-    {
-        _userRepository = userRepository;
+    public UseCaseCreatePassenger(IPassengerRepository passengerRepository, IMapper mapper) {
+        _passengerRepository = passengerRepository;
         _mapper = mapper;
     }
 
-    public DtoOutputUser Execute(DtoInputCreatePassenger input)
-    {
-        var dbUser = _userRepository.CreatePassenger(input.Username, input.Password, input.Email, input.Birthdate, input.IsBanned, input.PhoneNumber,
-            input.LastName, input.FirstName, input.Gender, input.AddressId);
-        return _mapper.Map<DtoOutputUser>(dbUser);
+    public DtoOutputPassenger Execute(DtoInputCreatePassenger input) {
+        var dbUser = _passengerRepository.CreatePassenger(input.Username, input.Password, input.Email, input.Birthdate,
+            input.IsBanned, input.PhoneNumber, input.LastName, input.FirstName, input.Gender, input.AddressId);
+        return _mapper.Map<DtoOutputPassenger>(dbUser);
     }
 }
--- Application/UseCases/Users/Passenger/UseCaseUpdatePassenger.cs
+++ Application/UseCases/Users/Passenger/UseCaseUpdatePassenger.cs
@@ -1,5 +1,22 @@
-﻿namespace Application.UseCases.Users.Passenger; 
+﻿using Application.UseCases.Users.Passenger.Dtos;
+using Application.UseCases.Users.User.Dtos;
+using AutoMapper;
+using Infrastructure.Ef.Users.Passenger;
+
+namespace Application.UseCases.Users.Passenger; 
 
 public class UseCaseUpdatePassenger {
-    
+    private readonly IPassengerRepository _passengerRepository;
+    private readonly IMapper _mapper;
+
+    public UseCaseUpdatePassenger(IPassengerRepository passengerRepository, IMapper mapper) {
+        _passengerRepository = passengerRepository;
+        _mapper = mapper;
+    }
+
+    public DtoOutputPassenger Execute(DtoInputUpdatePassenger input) {
+        var dbPassenger = _passengerRepository.UpdatePassenger(input.Id,input.Username, input.Password, input.Email, 
+            input.Birthdate, input.IsBanned, input.PhoneNumber, input.LastName, input.FirstName, input.Gender, input.AddressId);
+        return _mapper.Map<DtoOutputPassenger>(dbPassenger);
+    }
 }
