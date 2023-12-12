e06fd30840150b1fadd0f64236c8df0773d1b27f
Antoine <hauret.antoine@gmail.com>
2023-12-12 11:29:26 +0100

    Creation of PassengerRepository.cs and IPassengerRepository.cs (create and update)

--- Infrastructure/Ef/Users/Passenger/IPassengerRepository.cs
+++ Infrastructure/Ef/Users/Passenger/IPassengerRepository.cs
@@ -1,5 +1,11 @@
-﻿namespace Infrastructure.Ef.Users.Passenger; 
+﻿using Infrastructure.Ef.DbEntities;
+
+namespace Infrastructure.Ef.Users.Passenger; 
 
 public interface IPassengerRepository {
+    DbUser CreatePassenger(string username, string password, string email, DateTime birthdate, bool isbanned, string phoneNumber,
+        string lastName, string firstName, string gender, int addressId);
     
+    DbUser UpdatePassenger(int id, string username, string password, string email, DateTime birthdate, bool isbanned, string phoneNumber,
+        string lastName, string firstName, string gender, int addressId);
 }
