c6a66bfe9567fe151dfd64745f8724c924eea9a3
Antoine <hauret.antoine@gmail.com>
2023-12-12 11:11:36 +0100

    Bug fix for the insertFile

--- WayMateInsert.sql
+++ WayMateInsert.sql
@@ -4,17 +4,17 @@ INSERT INTO address (street, postalCode, city, number) VALUES
     ('Star Avenue', '54321', 'Galaxy City', '42'),
     ('Millennium Square', '67890', 'Spaceport', '7');
 GO
--- Inserting users
-INSERT INTO users (userType, username, password, email, birthDate, isBanned, phoneNumber, lastName, firstName, gender, addressId, carPlate) VALUES
-    ('Driver', 'luke_skywalker', 'password123', 'luke@example.com', '1980-05-25', 0, '1234567890', 'Skywalker', 'Luke', 'Male', 1, 'ABC123'),
-    ('Passenger', 'leia_organa', 'pass456', 'leia@example.com', '1983-09-19', 0, '9876543210', 'Organa', 'Leia', 'Female', 2, 'DEF456'),
-    ('Admin', 'han_solo', 'soloPass', 'han@example.com', '1977-04-29', 0, '6543210987', 'Solo', 'Han', 'Male', 3, 'GHI789');
-GO
 -- Inserting cars
 INSERT INTO car (plateNumber, model, nbSeats, brand, carType, fuelType) VALUES
     ('ABC123', 'Millennium Falcon', 5, 'Corellian Engineering Corporation', 0, 0),
     ('DEF456', 'X-34 Landspeeder', 2, 'SoroSuub Corporation', 0, 0),
     ('GHI789', 'Slave I', 3, 'Kuat Systems Engineering', 0, 0);
+GO
+-- Inserting users
+INSERT INTO users (userType, username, password, email, birthDate, isBanned, phoneNumber, lastName, firstName, gender, addressId, carPlate) VALUES
+    ('Driver', 'luke_skywalker', 'password123', 'luke@example.com', '1980-05-25', 0, '1234567890', 'Skywalker', 'Luke', 'Male', 1, 'ABC123'),
+    ('Passenger', 'leia_organa', 'pass456', 'leia@example.com', '1983-09-19', 0, '9876543210', 'Organa', 'Leia', 'Female', 2, 'DEF456'),
+    ('Admin', 'han_solo', 'soloPass', 'han@example.com', '1977-04-29', 0, '6543210987', 'Solo', 'Han', 'Male', 3, 'GHI789');
 GO
 -- Inserting trips
 INSERT INTO trip (idDriver, smoke, priceKm, luggage, petFriendly, date, occupiedSeats, idStartingPoint, idDestination) VALUES
