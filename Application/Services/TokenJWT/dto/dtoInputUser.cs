3e1677485234fdb409b5d95c6ede1a07164a1c05
Antoine <hauret.antoine@gmail.com>
2023-12-13 18:00:42 +0100

    Creation of the dto that will be use for the service token

---
+++ Application/Services/TokenJWT/dto/dtoInputUser.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Enums;
+
+namespace Application.Services.TokenJWT.dto; 
+
+public class DtoInputUser {
+    public string Username { get; set; }
+    public UserType UserType { get; set; }
+    public string Password { get; set; }
+}
