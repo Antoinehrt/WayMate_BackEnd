21b64388c1a50468a17ff3103ec19912be617ef9
SanoO0 <mathiasmanzano37@gmail.com>
2023-12-16 02:43:52 +0100

    Create UseCaseFetchAllTrip with dto

---
+++ Application/UseCases/Trip/Dtos/DtoOutputTrip.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.UseCases.Trip.Dtos;
+
+public class DtoOutputTrip
+{
+    public int Id { get; set; }
+    public int IdDriver { get; set; }
+    public bool Smoke { get; set; }
+    public float PriceKm { get; set; }
+    public float Luggage { get; set; }
+    public bool PetFriendly { get; set; }
+    public DateTime Date { get; set; }
+    public int OccupiedSeats { get; set; }
+    public int IdStratingPoint { get; set; }
+    public int IdDestination { get; set; }
+}
