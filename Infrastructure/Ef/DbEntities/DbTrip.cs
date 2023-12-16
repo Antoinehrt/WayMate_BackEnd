457c531cb5aa2f6bb2311f264d69ec383ddb3a4f
SanoO0 <mathiasmanzano37@gmail.com>
2023-12-16 02:36:58 +0100

    Create DbEntities : DbTrip

---
+++ Infrastructure/Ef/DbEntities/DbTrip.cs
@@ -0,0 +1,16 @@
+﻿namespace Infrastructure.Ef.DbEntities;
+
+public class DbTrip
+{
+    public int Id { get; set; }
+    public int IdDriver { get; set; }
+    public bool Smoke { get; set; }
+    public float PriceKm { get; set; }
+    public float Luggage { get; set; }
+    public bool PetFriendly { get; set; }
+    public DateTime Date { get; set; }
+    public int OccupiedSeats { get; set; }
+    public int IdStratingPoint { get; set; }
+    public int IdDestination { get; set; }
+
+}
