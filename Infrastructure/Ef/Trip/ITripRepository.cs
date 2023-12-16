3e776ffe11734ab808d556f721fb9c16d596c429
SanoO0 <mathiasmanzano37@gmail.com>
2023-12-16 02:37:41 +0100

    Create Trip repository

---
+++ Infrastructure/Ef/Trip/ITripRepository.cs
@@ -0,0 +1,8 @@
+﻿using Infrastructure.Ef.DbEntities;
+
+namespace Infrastructure.Ef.Trip;
+
+public interface ITripRepository
+{
+    IEnumerable<DbTrip> FetchAll();
+}
---
+++ Infrastructure/Ef/Trip/TripRepository.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Ef.DbEntities;
+
+namespace Infrastructure.Ef.Trip;
+
+public class TripRepository : ITripRepository
+{
+    private readonly WaymateContext _context;
+
+    public TripRepository(WaymateContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<DbTrip> FetchAll()
+    {
+        return _context.Trip.ToList();
+    }
+}
