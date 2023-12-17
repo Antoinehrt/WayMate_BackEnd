4d18c0b8ae29e1a6772e8f8503b60ba77b57fa4a
tchagisvi <118187580+tchagisvi@users.noreply.github.com>
2023-12-17 04:36:07 +0100

    Creating of Session

---
+++ Infrastructure/Ef/Session/SessionService.cs
@@ -0,0 +1,76 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Domain.Entities.Users;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Infrastructure.Ef.Session;
+
+public class SessionService : ISessionService
+{
+    private const double EXPIRY_DURATION_MINUTES = 1440;
+    
+    public string BuildToken(string key, string issuer, User user, string function)
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim(ClaimTypes.Name, user.Username),
+            new Claim(ClaimTypes.Role, function),
+            new Claim(ClaimTypes.NameIdentifier,
+                Guid.NewGuid().ToString())
+        };
+
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+        var tokenDescriptor = new JwtSecurityToken(issuer, issuer, claims,
+            expires: DateTime.Now.AddMinutes(EXPIRY_DURATION_MINUTES), signingCredentials: credentials);
+        return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+    }
+
+    public string BuildTokenPublic(string key, string issuer, User user, int id, string function)
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Email, user.IdUser.ToString()),
+            new Claim(ClaimTypes.Authentication, user.IsAdmin.ToString()),
+            new Claim(ClaimTypes.UserData, id.ToString()),
+            new Claim(ClaimTypes.Role, function),
+            new Claim(ClaimTypes.NameIdentifier,
+                Guid.NewGuid().ToString())
+        };
+
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+        var tokenDescriptor = new JwtSecurityToken(issuer, issuer, claims,
+            expires: DateTime.Now.AddMinutes(EXPIRY_DURATION_MINUTES), signingCredentials: credentials);
+        return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+    }
+
+    public bool IsTokenValid(string key, string issuer, string token)
+    {
+        var mySecret = Encoding.UTF8.GetBytes(key);
+        var mySecurityKey = new SymmetricSecurityKey(mySecret);
+        var tokenHandler = new JwtSecurityTokenHandler();
+        try
+        {
+            tokenHandler.ValidateToken(token,
+                new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = issuer,
+                    ValidAudience = issuer,
+                    IssuerSigningKey = mySecurityKey,
+                }, out SecurityToken validatedToken);
+        }
+        catch
+        {
+            return false;
+        }
+
+        return true;
+    
+    }
+}
