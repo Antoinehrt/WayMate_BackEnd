e829c3101b74499b4e6b3b5d89dafb9b633c2f22
SanoO0 <mathiasmanzano37@gmail.com>
2023-12-17 22:29:30 +0100

    Error in type Luggage

--- Application/UseCases/Trip/Dtos/DtoOutputTrip.cs
+++ Application/UseCases/Trip/Dtos/DtoOutputTrip.cs
@@ -6,7 +6,7 @@ public class DtoOutputTrip
     public int IdDriver { get; set; }
     public bool Smoke { get; set; }
     public float PriceKm { get; set; }
-    public float Luggage { get; set; }
+    public bool Luggage { get; set; }
     public bool PetFriendly { get; set; }
     public DateTime Date { get; set; }
     public int OccupiedSeats { get; set; }
