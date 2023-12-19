38b6b81fb40d81488864b720d3dec2cfc6b9d0ed
SanoO0 <mathiasmanzano37@gmail.com>
2023-12-19 16:52:58 +0100

    Add description in FuelType enum

--- Domain/Enums/FuelType.cs
+++ Domain/Enums/FuelType.cs
@@ -1,5 +1,24 @@
-﻿namespace Domain.Enums; 
+﻿using System.ComponentModel;
 
-public enum FuelType {
-    Electric, Diesel, Gasoline,  Hybrid, Lpg, Other 
+namespace Domain.Enums; 
+
+public enum FuelType: int  {
+    [Description("ELECTRIC")]
+    Electric = 0,
+    
+    [Description("DIESEL")]
+    Diesel = 0,
+    
+    [Description("GASOLINE")]
+    Gasoline = 0,
+    
+    [Description("HYBRID")]
+    Hybrid = 0,
+    
+    [Description("LPG")]
+    Lpg = 0,
+    
+    [Description("OTHER")]
+    Other = 0
+
 }
