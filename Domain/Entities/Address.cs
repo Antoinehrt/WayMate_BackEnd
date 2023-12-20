c0b564ff3a8d2ca37f8574e5e0185a8e08464615
tchagisvi <118187580+tchagisvi@users.noreply.github.com>
2023-12-20 23:37:11 +0100

    modiification of entity Adress

--- Domain/Entities/Address.cs
+++ Domain/Entities/Address.cs
@@ -5,9 +5,9 @@ namespace Domain.Entities;
 
 public class Address {
     public string Street { get; set; }
+    public string Number { get; set; }
     public string PostalCode { get; set; }
     public string City { get; set; }
-    public string Number { get; set; }
     private User _passenger;
    
 }
