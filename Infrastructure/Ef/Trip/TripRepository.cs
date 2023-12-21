257fdab83e90627b70d94808e655195bf21fb3bf
SanoO0 <mathiasmanzano37@gmail.com>
2023-12-21 18:14:57 +0100

    Create method to delete and update a trip

--- Infrastructure/Ef/Trip/TripRepository.cs
+++ Infrastructure/Ef/Trip/TripRepository.cs
@@ -35,4 +35,40 @@ public class TripRepository : ITripRepository
 
         return trip;
     }
+
+    public bool Delete(int id)
+    {
+        var tripToDelete = _context.Trip.FirstOrDefault(t => t.Id == id);
+
+        if (tripToDelete == null)
+        {
+            throw new KeyNotFoundException($"Trip with id {id} has not been found");
+        }
+
+        _context.Trip.Remove(tripToDelete);
+        _context.SaveChanges();
+
+        return true;
+    }
+
+    public bool Update(int id, int idDriver, bool smoke, double price, bool luggage, bool petFriendly, DateTime date,
+        string driverMessage, bool airConditioning, int idStartingPoint, int idDestination)
+    {
+        var tripToUpdate = _context.Trip.FirstOrDefault(t => t.Id == id);
+        if(tripToUpdate == null) return false;
+
+        tripToUpdate.IdDriver = idDriver;
+        tripToUpdate.Smoke = smoke;
+        tripToUpdate.Price = price;
+        tripToUpdate.Luggage = luggage;
+        tripToUpdate.PetFriendly = petFriendly;
+        tripToUpdate.Date = date;
+        tripToUpdate.DriverMessage = driverMessage;
+        tripToUpdate.AirConditioning = airConditioning;
+        tripToUpdate.IdStartingPoint = idStartingPoint;
+        tripToUpdate.IdDestination = idDestination;
+
+        _context.SaveChanges();
+        return true;
+    }
 }
